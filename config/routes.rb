5f897dd7cb1fe9bb48c25162a87fcf0d52dc3a68
Serhii Budnik <serhiibudnik@gmail.com>
2017-10-28 20:39:07 +0300

    add view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :home, only: %i[index]
 
-  root to: "home#index"
+  root to: 'web/home#index'
 
   mount_devise_token_auth_for 'User', at: 'auth',
                                       controllers: {
